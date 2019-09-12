b75bc0b48571c2dd1a9320c31151b2dec6975765
kangkai1314 <1253393765@qq.com>
2019-09-12 18:42:59 +0800

    add login

--- hello/src/views/login/login.vue
+++ hello/src/views/login/login.vue
@@ -3,15 +3,15 @@
       <div class="login-header" style="height: 100px">
       <el-image></el-image>
       </div>
-      <el-form inline="true" label-width="120px" >
-        <el-form-item label="用户名" style="width: 100%">
-          <el-input></el-input>
+      <el-form :inline="true" label-width="120px" :rules="rules" :model="login" ref="login" >
+        <el-form-item label="用户名" style="width: 100%" prop="username">
+          <el-input v-model="login.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" style="width: 100%">
-          <el-input></el-input>
+        <el-form-item label="密码" style="width: 100%" prop="password">
+          <el-input v-model="login.password"></el-input>
         </el-form-item>
         <el-form-item style="width: 100%;padding-left: 40px">
-          <el-button>登录</el-button>
+          <el-button @click="logined('login')">登录</el-button>
           <el-button>注册</el-button>
         </el-form-item>
       </el-form>
@@ -25,7 +25,37 @@
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      rules: {
+        username: [{
+          required: true, message: '用户名不能为空', trigger: 'blur'
+        }],
+        password: [{
+          required: true, message: '密码不能为空', trigger: 'blur'
+        }]
+      },
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    logined: function (formName) {
+      console.log(this.login)
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log(this.login)
+        } else {
+          this.$message.error('error')
+          return false
+        }
+      })
+    }
+
+  }
 }
 </script>
 
